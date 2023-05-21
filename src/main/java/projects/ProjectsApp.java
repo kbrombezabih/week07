00e6c2576d70b2b994d2e5e120de11e62de33c92
kbrombezabih <kbzeaxum@gmail.com>
2023-05-20 23:55:22 -0700

    SQL UPDATE and DELETE

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -14,7 +14,8 @@ public class ProjectsApp {
   private Project curProject;
   
   // @formatter:off
-  private List<String> operations = List.of("1) Add a project", "2) List projects", "3) Select a project");
+  private List<String> operations = List.of("1) Add a project", "2) List projects", "3) Select a project", 
+                                            "4) Update project details", "5) Delete a project");
   // @formatter:on
   private Scanner scanner = new Scanner(System.in);
 
@@ -44,6 +45,12 @@ public class ProjectsApp {
           case 3:
             selectProject();
             break;
+          case 4:
+            updateProjectDetails();
+            break;
+          case 5:
+            deleteProject();
+            break;
           default:
             System.out.println("\n" + selection + " is not a valid selection. Try again.");
             break;
@@ -54,6 +61,40 @@ public class ProjectsApp {
         //e.printStackTrace();
       }
     }
+    
+  }
+
+  private void deleteProject() {
+    listProjects();
+    
+    Integer projectId = getIntInput("\nEnter a project ID to select a project");
+    projectService.deleteProject(projectId);
+
+  }
+
+  private void updateProjectDetails() {
+    
+    if(Objects.isNull(curProject)) {
+      System.out.println("\nPlease select a project");
+      return;
+    }
+    String projectName = getStringInput("Enter the project name [" + curProject.getProjectName() + "]");
+    BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours [" + curProject.getEstimatedHours() + "]");
+    BigDecimal actualHours = getDecimalInput("Enter the actual hours [" + curProject.getActualHours() + "]");
+    Integer difficulty = getIntInput("Enter the project difficulty (1-5) [" + curProject.getDifficulty() + "]");
+    String notes = getStringInput("Enter the project notes [" + curProject.getNotes() + "]");
+     
+    Project project = new Project();
+    project.setProjectName(Objects.isNull(projectName) ? curProject.getProjectName() : projectName);
+    project.setEstimatedHours(Objects.isNull(estimatedHours) ? curProject.getEstimatedHours() : estimatedHours);
+    project.setActualHours(Objects.isNull(actualHours) ? curProject.getActualHours() : actualHours);
+    project.setDifficulty(Objects.isNull(difficulty) ? curProject.getDifficulty() : difficulty);
+    project.setNotes(Objects.isNull(notes) ? curProject.getNotes() : notes);
+    
+    project.setProjectId(curProject.getProjectId());
+    projectService.modifyProjectDetails(project);
+    curProject = projectService.fetchProjectById(curProject.getProjectId());
+    
     
   }
 
